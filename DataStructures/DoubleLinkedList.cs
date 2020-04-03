5851815bee62c06ce8e77f003df7c782f89601a2
Anastasia Kuden <anastasya.kuden@gmail.com>
2020-04-04 00:49:14 +0300

    Add DoubleLinkedList

---
+++ DataStructures/DoubleLinkedList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    public class DoubleLinkedList
+    {
+        private L2Node root;
+        private L2Node end;
+        public int Length { get; private set; }
+
+
+    }
+}
